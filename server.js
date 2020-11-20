95938ffb87093ee1e7fa199e8d7da002483502dd
magnus <magnus@protonmail.com>
2020-11-20 23:21:22 +0000

    working

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
     var roomResult = getRooms();
     var roomData = roomResult["roomData"];
     var roomSysidList = roomResult["roomSysidList"];
-	  var cityData = roomResult["cityData"];
-	  var roomSysidName = roomResult["roomSysidName"];
+    var cityData = roomResult["cityData"];
+    var roomSysidName = roomResult["roomSysidName"];
     // rows
     var rowResult = getRows(roomSysidList);
     var rowRel = rowResult["rowRel"];
@@ -13,42 +13,42 @@
     var rackResult = getRacks(rowSysidList);
     var rackRel = rackResult["rackRel"];
     // send data to client
-	  data.cityData = cityData;
+    data.cityData = cityData;
     data.roomData = roomData;
-	  data.roomSysidName = roomSysidName;
+    data.roomSysidName = roomSysidName;
     data.rowRel = rowRel;
     data.rackRel = rackRel;
-		//
+    //
     function getRooms(){
         var roomData = {};
         var roomSysidList = [];
-			  var cityData = {};
-			  var city;
-			  var roomSysidName = {};
-			  var roomName;
-			  var roomSysid;
+        var cityData = {};
+        var city;
+        var roomSysidName = {};
+        var roomName;
+        var roomSysid;
         grRooms = new GlideRecord("cmdb_ci_computer_room");
         grRooms.query();
         while (grRooms.next()) {
-					  roomName = grRooms.name.getValue();
-					  roomSysid = grRooms.sys_id.getValue();
-					  city = grRooms.location.city.getValue();
-					  if (!city){
-							city = "City Missing";
-						}
+            roomName = grRooms.name.getValue();
+            roomSysid = grRooms.sys_id.getValue();
+            city = grRooms.location.city.getValue();
+            if (!city){
+                city = "City Missing";
+            }
             roomData[roomName] = roomSysid;
             roomSysidList.push(roomSysid);
-					  if (!cityData.hasOwnProperty(city)){
-							cityData[city] = [];
-						}
-						cityData[city].push(grRooms.sys_id.getValue())
-					  roomSysidName[roomSysid] = roomName;
+            if (!cityData.hasOwnProperty(city)){
+                cityData[city] = [];
+            }
+            cityData[city].push(grRooms.sys_id.getValue())
+            roomSysidName[roomSysid] = roomName;
         }
         return({
-					  "cityData": cityData,
+            "cityData": cityData,
             "roomData": roomData,
             "roomSysidList": roomSysidList,
-					  "roomSysidName": roomSysidName
+            "roomSysidName": roomSysidName
         })
     }
     function getRows(roomSysidList) {
@@ -92,6 +92,5 @@
         return({
             "rackRel": rackRel
         })
-
     }
 })();
